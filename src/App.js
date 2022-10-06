0e73ec90b0e34f2e1fe99bc503bd78539266b7f3
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2022-10-06 22:53:51 +0530

    testimonials

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Testimonials from './Components/Testimonials';
+import Testimonialsection from './Components/Testimonialsection';
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+     <Testimonialsection />
     </div>
   );
 }
