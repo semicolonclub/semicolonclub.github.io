ebcf391063a66582d5a9c67e190891bcdeef899e
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-10-08 22:37:38 +0530

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,7 +4,19 @@ import '../Styles/Hero.css';
 function Hero() {
   return (
     <div>
-      
+        <div class="top">
+        <div class="logo">
+            <img src="semicolon_logo.jpg" alt="logo" id="logo"/>
+        </div>
+        <div class="wrap">
+            <div class="landing">
+                <span class="changing_text"></span>
+            </div>
+        </div>
+        <div class="upcoming">
+            <a href="/" class="btn">Upcoming Events</a>
+        </div>
+    </div>
     </div>
   )
 }
