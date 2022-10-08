4f9b42ee48659b39d3b887be4702788e723ac808
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-10-08 22:42:39 +0530

    Create HeroScript.js

---
+++ src/Components/HeroScript.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".changing_text",{
+    strings:["Welcome to Semicolon.","This is the official coding club of NIT Srinagar.","If you love coding,","You are at the right place."],
+    typeSpeed: 50,
+    backSpeed: 11,
+    loop: true
+});
