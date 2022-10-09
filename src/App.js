9057aaee56adf6ecc379ad9dde38d68483a3f62b
amansinghbisht029 <83057532+amansinghbisht029@users.noreply.github.com>
2022-10-09 19:49:55 +0530

    updated upcoming.js and upcoming.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import './App.css';
 import Testimonials from './Components/Testimonials';
 import Testimonialsection from './Components/Testimonialsection';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Upcoming from './Components/Upcoming';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Footer />
+      <Upcoming/>
       <Testimonialsection />
+      <Footer />
     </div>
   );
 }
