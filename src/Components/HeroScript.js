154730bc3d8432b8065384c16b744495a183373a
harsh-nitsgr <91006836+harsh-nitsgr@users.noreply.github.com>
2022-10-12 16:54:09 +0530

    Delete HeroScript.js

--- src/Components/HeroScript.js
+++
@@ -1,6 +0,0 @@
-var typed=new Typed(".changing_text",{
-    strings:["Welcome to Semicolon.","This is the official coding club of NIT Srinagar.","If you love coding,","You are at the right place."],
-    typeSpeed: 50,
-    backSpeed: 11,
-    loop: true
-});
