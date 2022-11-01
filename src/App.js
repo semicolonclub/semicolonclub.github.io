852a356917f9d7aa96cc85d85e2d19dcc683bc07
AASHISH KUMAR NAYAK <kumaraashish57627@gmail.com>
2022-11-01 20:29:38 +0530

    TeamPage is Updated.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import EventPage from './Components/EventPage';
 import HomePage from './Components/HomePage';
+import TeamPage from './Components/TeamPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
-      {/* <EventPage /> */}
+       <HomePage />
+       {/* <EventPage /> 
+       <TeamPage/> */}
     </div>
   );
 }
