8d70e821c53606b4d7e6024ab7bd19339aed7faa
AASHISH KUMAR NAYAK <kumaraashish57627@gmail.com>
2022-11-01 20:31:00 +0530

    Team section is updated.

---
+++ src/Components/TeamCards.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import '../Styles/TeamCards.css';
+import pic1 from "../assets/pic1.jpg";
+
+
+
+function Team() {
+  return(
+    <div>
+         <div className='Team'><h1>Team</h1></div>
+   
+    <div class="container">
+   
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4> 
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+               
+            
+                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+    
+    <div class="card">
+        <div class="content">
+            <div class="imgBx">
+                <img src={pic1} alt=""/>
+            </div>
+            <div class="contentBx">
+                <h4>Name</h4>
+                <h5>Team</h5>
+            </div>
+            <div class="sci">
+            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-github" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+
+
+   
+
+</div>
+</div>
+      )
+} 
+
+export default Team
---
+++ src/Components/TeamPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import TeamCards from './TeamCards';
+
+function EventPage() {
+  return (
+    <div>
+      <Navbar />
+      <TeamCards />
+      <Footer />
+    </div>
+  )
+}
+
+export default EventPage;
