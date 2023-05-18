d3bd260c54ad688c746f33401926f1854a9c9fc5
amansinghbisht029 <83057532+amansinghbisht029@users.noreply.github.com>
2023-05-18 14:02:36 +0530

    gallerypage updated
    gallery images uploaded

--- src/Components/GalleryPage.js
+++ src/Components/GalleryPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "../Styles/Gallery.css";
-import pic1 from "../assets/pic1.jpg";
-import pic2 from "../assets/pic2.jpg";
-import pic3 from "../assets/pic3.jpg";
-import pic4 from "../assets/pic4.jpg";
+import fig1 from "../assets/gallery_img/fig1.JPG";
+import fig2 from "../assets/gallery_img/fig2.jpeg";
+import fig3 from "../assets/gallery_img/fig3.jpeg";
+import fig4 from "../assets/gallery_img/fig4.jpeg";
+import fig5 from "../assets/gallery_img/fig5.jpeg";
+import fig6 from "../assets/gallery_img/fig6.jpeg";
+import fig7 from "../assets/gallery_img/fig7.jpeg";
+import fig8 from "../assets/gallery_img/fig8.jpeg";
+import fig9 from "../assets/gallery_img/fig9.JPG";
 
 function GalleryPage() {
   return (
@@ -15,49 +20,49 @@ function GalleryPage() {
       <div className="gallery_row">
         <div className="column">
           <div className="img-hover-zoom">
-            <img src={pic1} style={{ width: "100%" }} alt="" />
+            <img src={fig1} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic3} style={{ width: "100%" }} alt="" />
+            <img src={fig2} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic2} style={{ width: "100%" }} alt="" />
-          </div>
-        </div>
-
-        <div className="column">
-          <div className="img-hover-zoom">
-            <img src={pic4} style={{ width: "100%" }} alt="" />
-          </div>
-          <div className="img-hover-zoom">
-            <img src={pic1} style={{ width: "100%" }} alt="" />
-          </div>
-          <div className="img-hover-zoom">
-            <img src={pic3} style={{ width: "100%" }} alt="" />
+            <img src={fig3} style={{ width: "100%" }} alt="" />
           </div>
         </div>
 
         <div className="column">
           <div className="img-hover-zoom">
-            <img src={pic3} style={{ width: "100%" }} alt="" />
+            <img src={fig4} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic1} style={{ width: "100%" }} alt="" />
+            <img src={fig5} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic4} style={{ width: "100%" }} alt="" />
+            <img src={fig6} style={{ width: "100%" }} alt="" />
           </div>
         </div>
 
         <div className="column">
           <div className="img-hover-zoom">
-            <img src={pic1} style={{ width: "100%" }} alt="" />
+            <img src={fig7} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic4} style={{ width: "100%" }} alt="" />
+            <img src={fig8} style={{ width: "100%" }} alt="" />
           </div>
           <div className="img-hover-zoom">
-            <img src={pic3} style={{ width: "100%" }} alt="" />
+            <img src={fig4} style={{ width: "100%" }} alt="" />
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="img-hover-zoom">
+            <img src={fig6} style={{ width: "100%" }} alt="" />
+          </div>
+          <div className="img-hover-zoom">
+            <img src={fig3} style={{ width: "100%" }} alt="" />
+          </div>
+          <div className="img-hover-zoom">
+            <img src={fig2} style={{ width: "100%" }} alt="" />
           </div>
         </div>
       </div>
