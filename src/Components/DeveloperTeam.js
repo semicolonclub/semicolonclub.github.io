21ccf5a501c487eb2edd120f15ad7f88e17d54f1
Zoyakhatry1 <83895340+Zoyakhatry1@users.noreply.github.com>
2023-05-18 23:12:12 +0530

    content

--- src/Components/DeveloperTeam.js
+++ src/Components/DeveloperTeam.js
@@ -11,120 +11,144 @@ function TeamSec() {
           <div class="team-card">
             <div class="img-wrapper">
               <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+                src={require("../assets/shiva.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Shiva Singh</p>
+            <p class="text-blk position">Managing Director</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
               <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+                src={require("../assets/aman.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Aman Kumar</p>
+            <p class="text-blk position">Secretary</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
               <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+                src={require("../assets/sanna.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Sanna Showkat</p>
+            <p class="text-blk position">Secretary, Content & Creative Team Lead</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/arnav.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Arnav Kashyap</p>
+            <p class="text-blk position">Operations Team Lead</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/piyush.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Piyush Kumar</p>
+            <p class="text-blk position">Associate Lead(Operations Team)</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/shreyas.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Shreyash Pal</p>
+            <p class="text-blk position">Associate Lead(Operations & Technical Team)</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/ishika.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Ishika Gupta</p>
+            <p class="text-blk position">Associate Lead(Operations Team & Content Team)</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/rishab.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Rishabh Gaud</p>
+            <p class="text-blk position">Technical Team Lead</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/pranav.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Pranav Sambyal</p>
+            <p class="text-blk position">Technical Team Lead</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/expert1.png"
+            <img
+                src={require("../assets/basar.png")}
                 class="team-img"
               />
             </div>
-            <p class="text-blk name">Maxwell Doe</p>
-            <p class="text-blk position">Instructor</p>
+            <p class="text-blk name">Basar Qari</p>
+            <p class="text-blk position">Associate Lead(Technical Team)</p>
+          </div>
+        </div>
+        <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+          <div class="team-card">
+            <div class="img-wrapper">
+            <img
+                src={require("../assets/sameer.png")}
+                class="team-img"
+              />
+            </div>
+            <p class="text-blk name">Syed Mohammed Sameer</p>
+            <p class="text-blk position">Associate Lead(Technical Team)</p>
+          </div>
+        </div>
+        <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
+          <div class="team-card">
+            <div class="img-wrapper">
+            <img
+                src={require("../assets/faizan.png")}
+                class="team-img"
+              />
+            </div>
+            <p class="text-blk name">Faizan Ali Mir</p>
+            <p class="text-blk position">PR & Advisory Team Lead</p>
           </div>
         </div>
       </div>
