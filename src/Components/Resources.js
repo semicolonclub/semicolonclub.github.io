adcf57986a2710071a05d2742b1b289866472484
Aashish Kumar Nayak <kumaraashish57627@gmail.com>
2023-05-19 23:04:45 +0530

    Resource page is updated.

--- src/Components/Resources.js
+++ src/Components/Resources.js
@@ -1,65 +1,68 @@
 import React from "react";
 import "../Styles/Resources.css";
 
+import pic1 from "../assets/resource_images/artificial inteligence.jpg";
+import pic2 from "../assets/resource_images/machine.jpg";
+import pic3 from "../assets/resource_images/html-css-javascript.jpg";
+import pic4 from "../assets/resource_images/git-commands.png";
 function Resources() {
   return (
     <div>
       <div class="header">
         <h1>Resources</h1>
 
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>Unlock the world of free knowledge and empower yourself with access to learning, tools, and more - all for free.</p>
       </div>
 
       <div class="row1-container">
-        <a href="" class="box box-down cyan">
+        <a href="https://www.coursera.org/learn/ai-for-everyone" class="box box-down cyan">
           <div>
-            <h2>ReactNative</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <h2>Artificial Intelligence (AI)</h2>
+            <p>Free certificstion course</p>
             <img
               className="img_resources"
-              src="https://assets.codepen.io/2301174/icon-supervisor.svg"
-              alt=""
+              src={pic1}
+              alt="image not found"
             />
           </div>
         </a>
-        <a href="#" class="box red">
+        <a href="https://www.coursera.org/learn/machine-learning" class="box red">
           <div>
             <h2>Machine Learning</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <p>Free certificstion course</p>
             <img
               className="img_resources"
-              src="https://assets.codepen.io/2301174/icon-team-builder.svg"
+              src={pic2}
               alt=""
             />
           </div>
         </a>
 
-        <a href="#" class="box box-down blue">
+        <a href="https://www.coursera.org/learn/html-css-javascript-for-web-developers" class="box box-down blue">
           <div>
-            <h2>Python</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <h2>html css javascript for web Developers</h2>
+            <p>Free certificstion course</p>
             <img
               className="img_resources"
-              src="https://assets.codepen.io/2301174/icon-calculator.svg"
+              src={pic3}
+              alt=""
+            />
+          </div>
+        </a>
+        <a href="https://www.youtube.com/watch?v=SWYqp7iY_Tc" class="box orange">
+          <div>
+            <h2>Git and github</h2>
+            <p> Tutorial video </p>
+            <img
+              className="img_resources"
+              src={pic4}
               alt=""
             />
           </div>
         </a>
       </div>
 
-      <div class="row2-container">
-        <a href="#" class="box orange">
-          <div>
-            <h2>Git Commands</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            <img
-              className="img_resources"
-              src="https://assets.codepen.io/2301174/icon-karma.svg"
-              alt=""
-            />
-          </div>
-        </a>
-      </div>
+      
     </div>
   );
 }
