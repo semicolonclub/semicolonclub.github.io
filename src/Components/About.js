dfb710c7f127abdaed6d04153ee10df9b6a67937
amansinghbisht029 <83057532+amansinghbisht029@users.noreply.github.com>
2023-06-03 07:46:17 +0530

    About and DeveloperTeam updated
    Minorchanges

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,55 +1,68 @@
-import React,{ useEffect } from 'react';
+import React, { useEffect } from "react";
 import pic1 from "../assets/semicolon.png";
 import pic2 from "../assets/gallery_img/fig3.jpeg";
-import '../Styles/About.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import "../Styles/About.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function About() {
   useEffect(() => {
     AOS.init();
-  }, [])
+  }, []);
 
   return (
-    <div className='parent'>
-       <div className='container'>   
-           <div className='flex' data-aos="flip-up" data-aos-easing="linear" data-aos-duration="1500">
-              <img className='who_img' src={pic1} alt="" />
-              <div>
-              <div className='heading'>
-            <h1>Who we are?</h1>
-           </div>
-              <p className='description'>Semicolon is the official coding club of National Institute of Technology Srinagar, a prestigious 
-technical institute in India, founded in a Summer of 2010, by the Department of Computer Science and Engineering under Dr.Mohammad Ahsan Chishti. Comprised of passionate programmers, developers, 
-and technology enthusiasts, we come together to foster a vibrant coding culture within our campus. 
-Semicolon serves as a platform for students to enhance their coding skills, explore new technologies, 
-and collaborate on innovative projects.
-</p>
-              </div>
-           </div>
-           <div className='flex1' data-aos="flip-down" data-aos-easing="linear" data-aos-duration="1500">
-              <div>
-              <div className='heading'>
-            <h1>What we do?</h1>
-           </div>
-              <p className='description'>Semicolon provides a platform for students to learn, collaborate, 
-and excel in the field of computer programming, through workshops, coding competitions, and 
-coding challenges, we aim to foster a vibrant coding culture within our college community. Our 
-club also organizes Non-Technical events, guest lectures and coding boot camps to facilitate 
-knowledge sharing and skill development. It also hosts one of the biggest Technical 
-fest in whole J&K valley named as “EXORDIUM”</p>
-              </div>
-              <img className='what_img' src={pic2} alt="" />
-           </div>
-       </div>
-
-
-
-
-
+    <div className="parent">
+      <div className="container">
+        <div
+          className="flex"
+          data-aos="flip-up"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <img className="who_img" src={pic1} alt="" />
+          <div>
+            <div className="heading">
+              <h1>Who are we?</h1>
+            </div>
+            <p className="description">
+              Semicolon is the official coding club of National Institute of
+              Technology Srinagar, a prestigious technical institute in India,
+              founded in a Summer of 2010, by the Department of Computer Science
+              and Engineering under Dr.Mohammad Ahsan Chishti. Comprised of
+              passionate programmers, developers, and technology enthusiasts, we
+              come together to foster a vibrant coding culture within our
+              campus. Semicolon serves as a platform for students to enhance
+              their coding skills, explore new technologies, and collaborate on
+              innovative projects.
+            </p>
+          </div>
+        </div>
+        <div
+          className="flex1"
+          data-aos="flip-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <div>
+            <div className="heading">
+              <h1>What we do?</h1>
+            </div>
+            <p className="description">
+              Semicolon provides a platform for students to learn, collaborate,
+              and excel in the field of computer programming, through workshops,
+              coding competitions, and coding challenges, we aim to foster a
+              vibrant coding culture within our college community. Our club also
+              organizes Non-Technical events, guest lectures and coding boot
+              camps to facilitate knowledge sharing and skill development. It
+              also hosts one of the biggest Technical fest in whole J&K valley
+              named as “EXORDIUM”
+            </p>
+          </div>
+          <img className="what_img" src={pic2} alt="" />
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default About
+export default About;
--- src/Components/DeveloperTeam.js
+++ src/Components/DeveloperTeam.js
@@ -5,15 +5,15 @@ import { NavLink } from "react-router-dom";
 function TeamSec() {
   return (
     <div className="team_sec">
-      <p className="teamsec_heading"> <span className="red">Our</span>  Team</p>
+      <p className="teamsec_heading">
+        {" "}
+        <span className="red">Our</span> Team
+      </p>
       <div className="responsive-container-block">
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src={require("../assets/shiva.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/shiva.png")} class="team-img" />
             </div>
             <p class="text-blk name">Shiva Singh</p>
             <p class="text-blk position">Managing Director</p>
@@ -22,10 +22,7 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src={require("../assets/amans.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/amans.png")} class="team-img" />
             </div>
             <p class="text-blk name">Aman Kumar</p>
             <p class="text-blk position">Secretary</p>
@@ -34,22 +31,18 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-              <img
-                src={require("../assets/sanna.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/sanna.png")} class="team-img" />
             </div>
             <p class="text-blk name">Sanna Showkat</p>
-            <p class="text-blk position">Secretary, Content & Creative Team Lead</p>
+            <p class="text-blk position">
+              Secretary, Content & Creative Team Lead
+            </p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/arnav.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/arnav.png")} class="team-img" />
             </div>
             <p class="text-blk name">Arnav Kashyap</p>
             <p class="text-blk position">Operations Team Lead</p>
@@ -58,10 +51,7 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/piyush.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/piyush.png")} class="team-img" />
             </div>
             <p class="text-blk name">Piyush Thakur</p>
             <p class="text-blk position">Associate Lead(Operations Team)</p>
@@ -70,34 +60,29 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/shreyas.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/shreyas.png")} class="team-img" />
             </div>
             <p class="text-blk name">Shreyash Pal</p>
-            <p class="text-blk position">Associate Lead(Operations & Technical Team)</p>
+            <p class="text-blk position">
+              Associate Lead(Operations & Technical Team)
+            </p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/ishika.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/ishika.png")} class="team-img" />
             </div>
             <p class="text-blk name">Ishika Gupta</p>
-            <p class="text-blk position">Associate Lead(Operations Team & Content Team)</p>
+            <p class="text-blk position">
+              Associate Lead(Operations Team & Content Team)
+            </p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/rishab.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/rishab.png")} class="team-img" />
             </div>
             <p class="text-blk name">Rishabh Gaud</p>
             <p class="text-blk position">Technical Team Lead</p>
@@ -106,10 +91,7 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/pranav.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/pranav.png")} class="team-img" />
             </div>
             <p class="text-blk name">Pranav Sambyal</p>
             <p class="text-blk position">Technical Team Lead</p>
@@ -118,22 +100,16 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/basar.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/basar.png")} class="team-img" />
             </div>
             <p class="text-blk name">Basar Qari</p>
-            <p class="text-blk position">Associate Lead(Technical Team)</p>
+            <p class="text-blk position">Technical Team Associate Lead(Former)</p>
           </div>
         </div>
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/sameer.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/sameer.png")} class="team-img" />
             </div>
             <p class="text-blk name">Syed Mohammed Sameer</p>
             <p class="text-blk position">Associate Lead(Technical Team)</p>
@@ -142,13 +118,10 @@ function TeamSec() {
         <div class="responsive-cell-block wk-desk-3 wk-ipadp-3 wk-tab-6 wk-mobile-12 team-card-container">
           <div class="team-card">
             <div class="img-wrapper">
-            <img
-                src={require("../assets/faizan.png")}
-                class="team-img"
-              />
+              <img src={require("../assets/faizan.png")} class="team-img" />
             </div>
             <p class="text-blk name">Faizan Ali Mir</p>
-            <p class="text-blk position">PR & Advisory Team Lead</p>
+            <p class="text-blk position">PR & Advisory Team Lead(Former)</p>
           </div>
         </div>
       </div>
