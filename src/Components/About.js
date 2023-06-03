3ce2de367814222a9280479a41541143c35fd14e
amansinghbisht029 <83057532+amansinghbisht029@users.noreply.github.com>
2023-06-03 21:15:49 +0530

    About Section Updated
    Framer motion added successfully

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,7 @@ import pic2 from "../assets/gallery_img/fig3.jpeg";
 import "../Styles/About.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { motion } from "framer-motion";
 
 function About() {
   useEffect(() => {
@@ -15,16 +16,52 @@ function About() {
       <div className="container">
         <div
           className="flex"
-          data-aos="flip-up"
-          data-aos-easing="linear"
-          data-aos-duration="1500"
+          // data-aos="flip-up"
+          // data-aos-easing="linear"
+          // data-aos-duration="1500"
         >
-          <img className="who_img" src={pic1} alt="" />
+          <motion.img
+            initial={{ x: -100, opacity: 0.8 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className="who_img"
+            src={pic1}
+            alt="Image"
+          />
           <div>
             <div className="heading">
-              <h1>Who are we?</h1>
+              <motion.h1
+                initial={{ x: 100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.2,
+                  x: { type: "spring", stiffness: 60 },
+                  opacity: { duration: 1 },
+                  ease: "easeIn",
+                  duration: 1,
+                }}
+                className="font-extrabold text-2xl mdl:text-3xl"
+              >
+                Who are we?
+              </motion.h1>
             </div>
-            <p className="description">
+            <motion.p
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.2,
+                x: { type: "spring", stiffness: 60 },
+                opacity: { duration: 0.6 },
+                ease: "easeIn",
+              }}
+              className="description"
+            >
               Semicolon is the official coding club of National Institute of
               Technology Srinagar, a prestigious technical institute in India,
               founded in a Summer of 2010, by the Department of Computer Science
@@ -34,20 +71,42 @@ function About() {
               campus. Semicolon serves as a platform for students to enhance
               their coding skills, explore new technologies, and collaborate on
               innovative projects.
-            </p>
+            </motion.p>
           </div>
         </div>
         <div
           className="flex1"
-          data-aos="flip-down"
-          data-aos-easing="linear"
-          data-aos-duration="1500"
+          // data-aos="flip-down"
+          // data-aos-easing="linear"
+          // data-aos-duration="1500"
         >
           <div>
             <div className="heading">
-              <h1>What we do?</h1>
+              <motion.h1
+                initial={{ x: -100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.2,
+                  x: { type: "spring", stiffness: 60 },
+                  opacity: { duration: 1 },
+                  ease: "easeIn",
+                  duration: 1,
+                }}
+              >
+                What we do?
+              </motion.h1>
             </div>
-            <p className="description">
+            <motion.p
+              initial={{ x: -100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.2,
+                x: { type: "spring", stiffness: 60 },
+                opacity: { duration: 0.6 },
+                ease: "easeIn",
+              }}
+              className="description"
+            >
               Semicolon provides a platform for students to learn, collaborate,
               and excel in the field of computer programming, through workshops,
               coding competitions, and coding challenges, we aim to foster a
@@ -56,9 +115,22 @@ function About() {
               camps to facilitate knowledge sharing and skill development. It
               also hosts one of the biggest Technical fest in whole J&K valley
               named as “EXORDIUM”
-            </p>
+            </motion.p>
           </div>
-          <img className="what_img" src={pic2} alt="" />
+          <motion.img
+            initial={{ x: 100, opacity: 0.8 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{
+              delay: 0.2,
+              x: { type: "spring", stiffness: 60 },
+              opacity: { duration: 1 },
+              ease: "easeIn",
+              duration: 1,
+            }}
+            className="what_img"
+            src={pic2}
+            alt="Image"
+          />
         </div>
       </div>
     </div>
